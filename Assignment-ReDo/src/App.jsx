261c58bb2e5a3ab61b9c57ebe67feffab8718903
omi1001 <monusingh2646@gmail.com>
2026-05-16 18:17:49 +0530

    initial commit

--- Assignment-ReDo/src/App.jsx
+++ Assignment-ReDo/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Navbar from "./components/layout/Navbar";
+import Hero from "./Components/section/Hero"
 
 function App() {
   return (
     <div className="min-h-screen bg-white font-sans antialiased text-slate-900">
      
       <Navbar />
+      <section><Hero/></section>
 
       
       </div>
